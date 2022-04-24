7802d66d67d2aef64a4637c0abc68ea5a7c3a410
plngu <j66k@yandex.ru>
2022-04-24 20:33:10 +0300

    first

--- backend/api/mixins.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import mixins, viewsets
-
-
-class CreateUpdateDestroyMixin(viewsets.ModelViewSet):
-    def get_permissions(self):
-        if self.action == 'retrieve':
-            return (ReadOnly(), )
-        return super().get_permissions()
--- backend/api/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-
-class AuthorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
-
-    def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
